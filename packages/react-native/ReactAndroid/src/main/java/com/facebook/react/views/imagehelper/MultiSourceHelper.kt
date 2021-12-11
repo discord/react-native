148171df5fb451843a97c0d353325cfcd3bbd0a3
mrkcsc <miguel@miguelgaeta.com>
2021-12-11 14:20:59 -0800

    Update RNA ImageView to support forced cached property to fix GIF flicker. Add ability to override mSource of the image source.

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/imagehelper/MultiSourceHelper.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/imagehelper/MultiSourceHelper.kt
@@ -59,8 +59,10 @@ public object MultiSourceHelper {
     for (source in sources) {
       val precision = Math.abs(1.0 - source.size / viewArea)
       if (precision < bestPrecision) {
-        bestPrecision = precision
-        best = source
+        if (!source.isForceCached) {
+          bestPrecision = precision
+          best = source
+        }
       }
       if (precision < bestCachePrecision &&
           source.cacheControl != ImageCacheControl.RELOAD &&
@@ -69,6 +71,10 @@ public object MultiSourceHelper {
               // a separate thread
               imagePipeline.isInDiskCacheSync(source.uri))) {
         bestCachePrecision = precision
+        bestCached = source
+      }
+      if (source.isForceCached) {
+        bestCachePrecision = 0.0
         bestCached = source
       }
     }
