375de1debd61a2d06a4ea18773e22c35575cfeb3
mrkcsc <miguel@miguelgaeta.com>
2022-04-19 09:37:48 -0700

    [RNA] Also enable E2E for older Android API 28

--- ReactAndroid/src/main/java/com/facebook/react/views/modal/ReactModalHostView.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/modal/ReactModalHostView.java
@@ -286,6 +286,7 @@ public class ReactModalHostView extends ViewGroup
 
     // Enabled Edge to Edge modal when transparent/translucent system UI.
     if (mTransparent && mStatusBarTranslucent) {
+      mDialog.getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
       mDialog.getWindow().setNavigationBarColor(
         ContextCompat.getColor(context, android.R.color.transparent));
       WindowCompat.setDecorFitsSystemWindows(mDialog.getWindow(), false);
