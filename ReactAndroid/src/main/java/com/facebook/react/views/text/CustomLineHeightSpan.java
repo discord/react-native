a30b417be6d726ba707d784cbf47b5a6bfe4d8ea
mrkcsc <miguel@miguelgaeta.com>
2023-01-25 12:02:16 -0800

    [RNA] Update CustomLineHeightSpan to delegate custom behavior to the consuming app.

--- ReactAndroid/src/main/java/com/facebook/react/views/text/CustomLineHeightSpan.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/text/CustomLineHeightSpan.java
@@ -10,11 +10,16 @@ package com.facebook.react.views.text;
 import android.graphics.Paint;
 import android.text.style.LineHeightSpan;
 
+import com.facebook.react.bridge.Callback;
+
 /**
  * We use a custom {@link LineHeightSpan}, because `lineSpacingExtra` is broken. Details here:
  * https://github.com/facebook/react-native/issues/7546
  */
 public class CustomLineHeightSpan implements LineHeightSpan, ReactSpan {
+
+  public static Callback chooseHeightOverride = null;
+
   private final int mHeight;
 
   public CustomLineHeightSpan(float height) {
@@ -23,10 +28,10 @@ public class CustomLineHeightSpan implements LineHeightSpan, ReactSpan {
 
   @Override
   public void chooseHeight(
-      CharSequence text, int start, int end, int spanstartv, int v, Paint.FontMetricsInt fm) {
+    CharSequence text, int start, int end, int spanstartv, int v, Paint.FontMetricsInt fm) {
 
-    if (OVERRIDE_LINE_HEIGHT) {
-      overrideLineHeight(fm);
+    if (chooseHeightOverride != null) {
+      chooseHeightOverride.invoke(fm, mHeight);
       return;
     }
 
@@ -51,43 +56,6 @@ public class CustomLineHeightSpan implements LineHeightSpan, ReactSpan {
       // Show all ascent, descent, bottom, as much top as possible
       fm.top = fm.bottom - mHeight;
     } else {
-      // Show proportionally additional ascent / top & descent / bottom
-      final int additional = mHeight - (-fm.top + fm.bottom);
-
-      // Round up for the negative values and down for the positive values  (arbitrary choice)
-      // So that bottom - top equals additional even if it's an odd number.
-      fm.top -= Math.ceil(additional / 2.0f);
-      fm.bottom += Math.floor(additional / 2.0f);
-      fm.ascent = fm.top;
-      fm.descent = fm.bottom;
-    }
-  }
-
-  private final static boolean OVERRIDE_LINE_HEIGHT = true;
-
-  /**
-   * Discord Story time!
-   *
-   * So since we decided to be _very_ flexible with channel names, users have decided that they were gonna name their channels
-   * shit like ‧͙⁺˚･༓☾text☽༓･˚⁺‧͙ | l̶̟̦͚͎̦͑̎m̵̮̥̫͕͚̜̱̫̺̪͍̯̉̂̔͌́̚̕a̶͖̫͍͇̯̯̭͎͋̅́̿́̕͘͘͝͝ò̶̧̢͎̃̋͆̉͠ | and other fun non-standard channel names.
-   *
-   * This caused issues with line heights, because the RN implementation decided that it would try as best as possible to
-   * fit the text within the lineHeight that was given to it by the react component, causing text to be shifted upward
-   * and look terrible (see: https://canary.discord.com/channels/281683040739262465/912423796915462154/101286117376867126
-   * for an example).
-   *
-   * We (Jerry + Charles) decided that to fix this issue, we would instead ignore lineHeights _only_ if the text
-   * height was larger than the lineHeight provided to it.
-   *
-   * This is a much simpler implementation that what was previously here.
-   *
-   * _IF_ the lineHeight is larger than the text height, we default to centering the text as much as possible within
-   * that line height.
-   */
-  private void overrideLineHeight(Paint.FontMetricsInt fm) {
-    int realTextHeight = fm.bottom - fm.top;
-
-    if (mHeight >= realTextHeight) {
       // Show proportionally additional ascent / top & descent / bottom
       final int additional = mHeight - (-fm.top + fm.bottom);
 
