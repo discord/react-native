8b7a743f687fe566b4b6b37cd0b0a3df3b2341af
Nicola Corti <ncor@meta.com>
2023-07-04 06:01:09 -0700

    Move Flipper integration to a separate Gradle module inside `ReactAndroid` (#37688)
    Summary:
    Pull Request resolved: https://github.com/facebook/react-native/pull/37688
    
    This moves the `ReactNativeFlipper` classes used to configure Flipper on Android from the template to
    a separate Gradle artifact that will be published under the coordinates:
    ```
    com.facebook.react:flipper-integration:0.73.x
    ```
    
    This reduces the footprint of Flipper on the app template and makes easier for user on 0.73 to migrate
    to Kotlin (as they will now have to migrate only 2 files rather than 4).
    
    Changelog:
    [Android] [Changed] - Move Flipper integration to a separate Gradle module inside `ReactAndroid`
    
    Reviewed By: huntie
    
    Differential Revision: D46441588
    
    fbshipit-source-id: e197f29b7386b52091b8d38ed09bbd8f74a997df

--- packages/rn-tester/android/app/src/main/java/com/facebook/react/uiapp/RNTesterApplication.java
+++ packages/rn-tester/android/app/src/main/java/com/facebook/react/uiapp/RNTesterApplication.java
@@ -19,6 +19,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
+import com.facebook.react.flipper.ReactNativeFlipper;
 import com.facebook.react.module.model.ReactModuleInfo;
 import com.facebook.react.module.model.ReactModuleInfoProvider;
 import com.facebook.react.shell.MainReactPackage;
