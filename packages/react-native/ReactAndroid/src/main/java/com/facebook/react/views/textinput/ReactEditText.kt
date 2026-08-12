6430e75ed0c6c6d8170db679469db2e6a53abd53
Jakub Kosmydel <jakub.kosmydel@discordapp.com>
2026-08-12 12:39:00 +0200

    clean up

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactEditText.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactEditText.kt
@@ -551,8 +551,6 @@ public open class ReactEditText public constructor(context: Context) : AppCompat
       isSingleLine = false
     }
 
-    // Keep placeholder ellipsize in sync when multiline flips. Single-line inputs should
-    // trail with "…" (matching iOS); multiline placeholders should still wrap.
     updatePlaceholderEllipsize()
 
     // We override the KeyListener so that all keys on the soft input keyboard as well as hardware
@@ -573,12 +571,6 @@ public open class ReactEditText public constructor(context: Context) : AppCompat
     updatePlaceholderEllipsize()
   }
 
-  /**
-   * On Android, long hints are clipped without an ellipsis unless [ellipsize] is set. Align with
-   * iOS by ellipsizing trailing placeholders on single-line inputs only.
-   *
-   * See https://github.com/facebook/react-native/issues/29663
-   */
   private fun updatePlaceholderEllipsize() {
     ellipsize =
         if (!isMultiline) {
